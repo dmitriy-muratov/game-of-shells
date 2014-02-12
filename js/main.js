a6464c13c6405bac364a9843207e7a6fb1cd7f5d
dmitriy_muratov <muratovdima@gmail.com>
2014-02-12 18:27:28 +0200

    code cleanup, adding some effects

--- js/main.js
+++ js/main.js
@@ -4,42 +4,31 @@ jQuery(document).ready(function(){
     var shell_item = $('.shell-item');
     var shell_count = shell_item.length;
     
-        //var random = Math.floor((Math.random()*shell_count) + 1);
-        //console.log(random);
-    
-    shell_array = new Array(shell_count);
-    shell_array = new Array(shell_item);
-    console.log(shell_array);
-    
-    
     // Start. Add ball in random item
-    $('#start-btn').click(function(){
     
-        function getRandom(num){
-            var my_num = Math.floor((Math.random()*shell_count));
-            return my_num;
-        }
-        
-        var hide_ball = function (){
-            var numRand = getRandom(shell_count);
-            shell_item.each(function(index) {
-                if(numRand == index) {
-                    $(this).append('<span id="ball" />')
-                    return false;
-                }
-            });
-        }
-        hide_ball();
+    function getRandom(num){
+        var my_num = Math.floor((Math.random()*shell_count));
+        return my_num;
+    }
     
-        $(this).unbind('click');
-    });// end Start
+    var hide_ball = function (){
+        var numRand = getRandom(shell_count);
+        shell_item.each(function(index) {
+            if(numRand == index) {
+                $(this).append('<span id="ball" />')
+                return false;
+            }
+        });
+    }
+    hide_ball();
+    
     
     shell_item.bind('click', function(index){
         if ($.contains( this, document.getElementById('ball'))) {
-            $('#middle .inner').prepend('<div class="win" />');
+            $('.win').addClass('active');
             $(this).addClass('lucky');
         } else {
-            $('#middle .inner').prepend('<div class="lose" />');
+            $('.lose').addClass('active');
             $(this).addClass('try-again');
         }
         
